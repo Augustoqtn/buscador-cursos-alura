d0d6f418be6ecf30e0ff2aa2eee850f3bd4b3730
Augusto <augustinhobrito542@gmail.com>
2022-05-26 19:12:15 -0300

    cria uma classe Buscador

--- buscar-cursos.php
+++ buscar-cursos.php
@@ -2,19 +2,16 @@
 
 require "vendor/autoload.php";
 
+use Alura\BuscadorDeCursos\Buscador;
 use GuzzleHttp\Client;
 use Symfony\Component\DomCrawler\Crawler;
 
-$client = new Client();
-$resposta = $client->request("GET", "https://www.alura.com.br/cursos-online-programacao/php");
+$client = new Client(["base_uri" => "https://www.alura.com.br/"]);
 
-$html = $resposta->getBody();
-
-$crawler = new Crawler();
-$crawler->addHtmlContent($html);
-
-$cursos = $crawler->filter(".card-curso__nome");
+$buscador = new Buscador($client, $crawler);
+$cursos = $buscador->buscar("cursos-online-programacao/php");
 
 foreach ($cursos as $curso) {
     echo $curso->textContent . PHP_EOL;
 }
+#4:22
---
+++ src/BuscadorDeCursos.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Alura\BuscadorDeCursos;
+
+use GuzzleHttp\ClientInterface;
+use Symfony\Component\DomCrawler\Crawler;
+
+class Buscador 
+{
+
+    private $httpClient;
+    private $crawler;
+
+    public function __construct(ClientInterface $httpClient, Crawler $crawler)
+    {
+        $this->httpClient = $httpClient;
+        $this->crawler = $crawler;
+    }
+
+    public function buscar(string $url): array
+    {
+        
+    }
+}
+
